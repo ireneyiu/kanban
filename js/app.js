0fb01f38481f1733824fc011ef6ef5105eb7ff24
ireneyiu <ireyiu@gmail.com>
2014-04-02 20:38:37 -0700

    Add kanban test data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-app.js
+(function ($) {
+ 
+    var kanban = [
+        { title: "Task 1", description: "Get this thing done!", status: "notStarted" },
+        { title: "Task 2", description: "Get this thing done!", status: "notStarted" },
+        { title: "Task 3", description: "Get this thing done!", status: "inProgress" },
+        { title: "Task 4", description: "Get this thing done!", status: "done" },
+        { title: "Task 5", description: "Get this thing done!", status: "notStarted" },
+        { title: "Task 6", description: "Get this thing done!", status: "done" },
+        { title: "Task 7", description: "Get this thing done!", status: "inProgress" },
+        { title: "Task 8", description: "Get this thing done!", status: "notStarted" }
+    ];
+ 
+} (jQuery));
