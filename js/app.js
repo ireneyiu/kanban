39ae7e27c13039b86b8c7c74cd6df3e8de4342f4
ireneyiu <ireyiu@gmail.com>
2014-04-03 20:14:36 -0700

    Add card model, kanban collection, and card view

--- js/app.js
+++ js/app.js
@@ -10,5 +10,28 @@
         { title: "Task 7", description: "Get this thing done!", status: "inProgress" },
         { title: "Task 8", description: "Get this thing done!", status: "notStarted" }
     ];
+
+    var Card = Backbone.Model.extend({
+      defaults: {
+        status: "notStarted"
+      }
+    });
+
+    var Kanban = Backbone.Collection.extend({
+      model: Card
+    });
+
+    var CardView = Backbone.View.extend({
+      tagname: "article",
+      className: "card-container",
+      template: $("#cardTemplate").html(),
+
+      render: function() {
+        var tmpl = _.template(this.template);
+
+        this.$el.html(tmpl(this.model.toJSON()));
+        return this;
+      }
+    });
  
 } (jQuery));
